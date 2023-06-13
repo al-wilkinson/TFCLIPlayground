18bc6d78ddd88705744aa185b426065b0ce6dc4c
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-06-13 18:55:57 +1000

    Try to output public IP

--- main.tf
+++ main.tf
@@ -131,3 +131,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 output "identity_principal_id" {
   value = azurerm_linux_virtual_machine.vm.identity.0.principal_id
 }  
+
+output "public_IP" {
+  value = azurerm_public_ip.pubip.*.ip_address
+}
